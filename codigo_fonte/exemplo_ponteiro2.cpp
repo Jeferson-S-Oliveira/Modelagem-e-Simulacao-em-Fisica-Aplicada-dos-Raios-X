3e6d47eb87ac1b386f8d596dfbf0f9aa667a1051
Jeferson-S-Oliveira <jeferson.oliveira.uerj@gmail.com>
2022-12-19 15:19:43 -0300

    Salvo em 2/12

---
+++ codigo_fonte/exemplo_ponteiro2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void) {
+    unsigned dado = 10;
+    unsigned *dptr; /* ponteiro unsigned dptr */
+    
+    printf("\n");
+    dptr = &dado;   /* recupera o endereco da variavel dado */
+    printf("Conteudo apontado por dptr: %d\n",*dptr);
+    
+    printf("Endereco de dado: %p\n", &dado);
+    printf("Endereco apontado por dptr: %p\n",dptr);
+    printf("Endereco dptr: %p\n", &dptr);
+    *dptr = 20;   /* -> dado = 20;    modifica indiretamente a variavel dado */
+    printf("Conteudo de dado: %d\n",dado);
+    printf("\n");
+}
---
+++ codigo_fonte/ponteiro_v.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void) {
+    int v[30], *vptr;
+    printf("v=%p\n",v);
+    vptr = v;
+    printf("vptr=%p\n",vptr);
+
+    *vptr = 10; // == *v == *(v+0) = v[0]
+    printf("v[0]=%d\n",v[0]);
+    *(vptr+20) = -10; // == *(v+20) = v[20]
+    printf("v[20]=%d\n",v[20]);
+    return 0;
+}
